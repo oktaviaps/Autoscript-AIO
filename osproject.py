af5e4f1dba1f961dfde01f919c50814e4370b7f2
oktaviaps <158389264+oktaviaps@users.noreply.github.com>
2024-09-13 20:58:32 +0700

    Add files via upload

---
+++ osproject.py
@@ -0,0 +1,17 @@
+import subprocess
+import os
+
+def main():
+    os.system('clear')
+
+    url = "https://github.com/DindaPutriFN/Autoscript/releases/download/1.10/setup.sh"
+    subprocess.run(['wget', '-O', 'setup.sh', '-q', url])
+
+    subprocess.run(['chmod', '+x', 'setup.sh'])
+
+    subprocess.run(['./setup.sh'])
+
+    subprocess.run(['rm', '-fr', 'setup.sh'])
+
+if __name__ == "__main__":
+    main()
